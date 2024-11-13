8a505db429e25ba7f82e69631ad362466e77c36e
FM-NIctJW <fernandomarlon2007.1976@gmail.com>
2024-11-12 22:06:29 -0600

    Archivos JAVA - Version 5

---
+++ Amigos.java
@@ -0,0 +1,11 @@
+public class Amigos {
+    public static void main(String[] args) {
+        System.out.println("Tus nuevos Mejores Amigos:");
+        System.out.println("\t- Sunny");
+        System.out.println("\t- Aubrey");
+        System.out.println("\t- Kel");
+        System.out.println("\t- Hero");
+        System.out.println("\t- Mari");
+        System.out.println("\t- Basil");
+    }
+}
